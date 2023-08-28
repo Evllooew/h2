6a709abda345bb9a29afc8800ba99d8c56880704
Evllooew <evllooev@icloud.com>
2023-08-28 14:39:17 +0300

    ДЗ Исправил

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import hogwarts.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Hogwarts[] hogwarts = {
+                new Gryffindor("harry ", 1, 2, 3, 4, 5),
+                new Gryffindor("Germany ", 5, 3, 3, 8, 2),
+                new Gryffindor("Ron ", 3, 7, 3, 4, 9),
+                new Hufflepuff("Drako ", 3, 7, 3, 2, 1),
+                new Hufflepuff("Graham ", 3, 7, 3, 2, 1),
+                new Hufflepuff("Gregory ", 3, 7, 3, 2, 1),
+                new Ravenclaw("Zacharias", 1, 6, 8, 3, 6, 2),
+                new Ravenclaw("Cedric", 2, 8, 1, 6, 9, 2),
+                new Ravenclaw("Justin", 2, 1, 1, 6, 9, 2),
+                new Slytherin("Cho", 1, 2, 8, 4, 1, 3),
+                new Slytherin("Padma", 9, 3, 2, 5, 1, 3),
+                new Slytherin("Marcus", 2, 2, 5, 2, 1, 3),
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        };
+        for (int i = 0; i < hogwarts.length; i++) {
+            for (int j = i + 1; j < hogwarts.length; j++)
+                hogwarts[i].compare(hogwarts[j]);
+
         }
+        Gryffindor harry = (Gryffindor) hogwarts[0];
+        Gryffindor germ = (Gryffindor) hogwarts[1];
+        harry.compare(germ);
+
     }
 }
